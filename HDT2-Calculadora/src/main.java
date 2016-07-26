4f761475fb19920272887a12928745ffa4a6e7fd
ChristianFML <mor15015@uvg.edu.gt>
2016-07-25 20:46:49 -0600

    Update main.java

--- HDT2-Calculadora/src/main.java
+++ HDT2-Calculadora/src/main.java
@@ -1,17 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Universidad del Valle de Guatemala
+ * @author Christian Morales, Jose Luis Méndez, Juan García
+ * Carne: 15015, 15021, 15046
+ * 25 de Julio del 2016
+ * Descripción: Esta clase utiliza la clase la calculadora y le da un stack tipo ArrayList o tipo Vector, donde puede funcionar con cualquiera
  */
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-/**
- *
- * @author Christian Morales
- */
 public class main {
     
     //Metodo para leer un archivo .TXT
@@ -26,9 +25,8 @@ public class main {
  
     public static void main(String[] args) throws IOException {
         
-        Stack pila = new StackArrayList<String>();
+        Stack pila = new StackVector<String>();
         Calculadora calc = new Calculadora(pila);
-        calc.evaluar(muestraContenido("C:/Users/Christian Morales/Desktop/archivo.txt") );
         String cadena = muestraContenido("C:/Users/Christian Morales/Desktop/archivo.txt");
         int resultado = calc.evaluar( cadena );
         System.out.println("El resultado es: "+resultado);
