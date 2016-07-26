ed499049ba28b4b1dd509dee80a03fa4809383cb
jose luis <jose luis@joseluismendez>
2016-07-25 20:27:16 -0600

    pruebenlo!!!!!

--- HDT2-Calculadora/src/StackVector.java
+++ HDT2-Calculadora/src/StackVector.java
@@ -10,33 +10,50 @@ import java.util.*;
 /**
  *
  * @author jose luis
+ * @param <E>
  */
 public class StackVector <E> 
 implements Stack<E>{
+    protected Vector<E> data;
+public StackVector (){
+    data = new Vector<E>();
+}
+     @Override
+	public void push(E item)
+	// post: the value is added to the stack
+	//          will be popped next if no intervening push
+	{
+		data.addElement(item);
+	}
 
-    @Override
-    public void push(E item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        @Override
+	public E pop()
+	// pre: stack is not empty
+	// post: most recently pushed item is removed and returned
+	{
+		return data.remove(size()-1);
+	}
 
-    @Override
-    public E pop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public E peek() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean empty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        @Override
+	public E peek()
+	// pre: stack is not empty
+	// post: top value (next to be popped) is returned
+	{
+		return data.get(size() - 1);
+	}
+	
+        @Override
+	public int size()
+	// post: returns the number of elements in the stack
+	{
+		return data.size();
+	}
+  
+        @Override
+	public boolean empty()
+	// post: returns true if and only if the stack is empty
+	{
+		return size() == 0;
+	}
     
 }
