d2cda3d6b30a6339377ba9947ba206479aa059f3
jose luis <jose luis@joseluismendez>
2016-07-25 20:23:42 -0600

    pruebenlo!!!!!

--- HDT2-Calculadora/src/StackVector.java
+++ HDT2-Calculadora/src/StackVector.java
@@ -11,54 +11,32 @@ import java.util.*;
  *
  * @author jose luis
  */
-public class StackVector {
-    public class StackArrayList <E>
- implements Stack<E>
-{
-	protected Vector<E> data;
+public class StackVector <E> 
+implements Stack<E>{
 
-	public StackArrayList()
-	// post: constructs a new, empty stack
-	{
-	}
+    @Override
+    public void push(E item) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-        @Override
-	public void push(E item)
-	// post: the value is added to the stack
-	//          will be popped next if no intervening push
-	{
-		data.addElement(item);
-	}
+    @Override
+    public E pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-        @Override
-	public E pop()
-	// pre: stack is not empty
-	// post: most recently pushed item is removed and returned
-	{
-		return data.remove(size()-1);
-	}
+    @Override
+    public E peek() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-        @Override
-	public E peek()
-	// pre: stack is not empty
-	// post: top value (next to be popped) is returned
-	{
-		return data.get(size() - 1);
-	}
-	
-        @Override
-	public int size()
-	// post: returns the number of elements in the stack
-	{
-		return data.size();
-	}
-  
-        @Override
-	public boolean empty()
-	// post: returns true if and only if the stack is empty
-	{
-		return size() == 0;
-	}
-}
+    @Override
+    public boolean empty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
