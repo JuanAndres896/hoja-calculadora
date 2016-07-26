0615e2b0cf3a9f2b2cc3dbeb52c2e7c7483b2de6
ChristianFML <mor15015@uvg.edu.gt>
2016-07-25 20:43:58 -0600

    Update Stack.java

--- HDT2-Calculadora/src/Stack.java
+++ HDT2-Calculadora/src/Stack.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Universidad del Valle de Guatemala
  * @author Christian Morales, Jose Luis Méndez, Juan García
  * Carne: 15015, 15021, 15046
- * 13 de Julio del 2016
+ * 25 de Julio del 2016
  * Descripción: Esta clase es la interfaz que se utilizará como modelo para simular el "Stack" que se encargará del manejo de los datos
  * @param <E>
  */
